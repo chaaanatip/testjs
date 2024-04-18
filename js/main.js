437d5ff1275e597d559ad93d5099031e8897929a
chaaanatip <110770012+chaaanatip@users.noreply.github.com>
2024-04-18 13:52:10 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//1.Class Book:
 class Book {
   constructor(title, author, yearPublished) {
     this.title = title || "no title"
@@ -9,9 +10,9 @@ class Book {
     return `Title: ${this.title}, Author: ${this.author}, Year Published: ${this.yearPublished}`
   }
 }
-
+//2.Create an object Book reference 'practice 1':
 const practice1 = new Book("JavaScript Practice 002", "Your Name", 2024)
-
+//3.Class Library:
 class Library {
   constructor(name) {
     this.name = name
@@ -32,14 +33,14 @@ class Library {
       .reduce((total, book) => total + book.pages, 0)
   }
 }
-
+//4.Create a 'Book' object and a 'Library' object for 'Practice 3':
 const book1 = new Book("Harry Potter", "J.K. Rowling", 400, "Fantasy")
 const book2 = new Book("The Hobbit", "J.R.R. Tolkien", 300, "Fantasy")
 
 const library = new Library("My Library")
 library.addBook(book1)
 library.addBook(book2)
-
+//5.Class Elevator:
 class Elevator {
   constructor(maxFloor, minFloor) {
     this.currentFloor = 0
@@ -69,17 +70,18 @@ class Elevator {
     console.log(`Current Floor: ${this.currentFloor}`)
   }
 }
-
+//6.Create a 'Elevator' object for 'Practice 5':
 const elevator = new Elevator(10, 0)
-
+//7.Class bus
 class Bus {
   constructor(busNumber, destinations) {
     this.busNumber = busNumber
     this.destinations = destinations
   }
 }
-
+//8.Create a 'Bus' object for 'Practice 7':
 const bus = new Bus(101, ["Stop1", "Stop2", "Stop3"])
+//9.Class BusStop:
 
 class BusStop {
   constructor(destinations) {
@@ -111,6 +113,6 @@ class BusStop {
     }))
   }
 }
-
+//10.Create a 'Bus' and 'BusStop' object for 'Practice 9':
 const busStop = new BusStop("Main Street")
 busStop.addBus(bus)
